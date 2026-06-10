8495fb2a9fd011236045322e584036e0f1c4bcc8
marcuschiu <marcuschiu9@gmail.com>
2026-06-09 20:05:12 -0500

    moved into public directory

--- update-production.sh
+++ update-production.sh
@@ -1,48 +1,7 @@
 #! /bin/bash
 
-hugo
-
-
-##############
-# 0. GIT WAY #
-##############
-
-cd public
-git add .
-git commit -am "initial commit"
-git push
-
 ssh aws << EOF
-  cd personal-website-two/public/
+  cd personal-website-3/public/
   git pull
 EOF
 
-
-##################
-# 1. REGULAR WAY #
-##################
-
-#ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
-#  rm -rf personal-website-two/
-#  mkdir personal-website-two
-#EOF
-#
-#scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public ec2-user@www.marcuschiu.com:~/personal-website-two
-
-
-#####################
-# 2. COMPRESSED WAY #
-#####################
-
-#ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
-#  rm -rf personal-website-two/
-#  mkdir personal-website-two
-#EOF
-#
-#tar czf public.tar.gz public
-#scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public.tar.gz ec2-user@www.marcuschiu.com:~/personal-website-two
-#ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
-#  cd personal-website-two
-#  tar -xvzf public.tar.gz
-#  rm public.tar.gz
-#EOF
